5e7b2f53b91dd654aed730b1d70fc4569c3512a8
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-17 00:30:52 +0300

    finished Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба
+// индекса чётные, и замените эти элементы на их квадраты.
+
+int[,] array = new int[4, 5];
+
+void FillinArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+
+
+void ChangeArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                array[i, j] = array[i, j] * array[i,j];
+            }
+        }
+    }
+}
+
+FillinArray();
+PrintArray();
+ChangeArray();
+System.Console.WriteLine();
+PrintArray();

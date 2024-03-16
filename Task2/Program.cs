ab5287fa46a43fcc96bb53f80de3cd4c36fbcc3a
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-17 00:44:51 +0300

    finished Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов,
+// находящихся на главной диагонали
+
+int[,] array = new int[5,5];
+
+void FillingArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(0,16);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i,j] + " ");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+int result()
+{
+    int sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if( i == j )
+            {
+                sum = sum + array[i, j];
+            }
+        }
+    }
+    return sum;
+}
+
+FillingArray();
+PrintArray();
+System.Console.WriteLine(result());

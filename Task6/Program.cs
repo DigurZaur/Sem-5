37e415a84ccc4dbb35ecc5507570086f55797c23
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-17 14:35:43 +0300

    finished Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введенных букв гласных.
+
+System.Console.WriteLine("Введите строчку");
+string userPrint = System.Console.ReadLine();
+
+string vowels = "aoiue";
+int count = 0;
+
+foreach (var item in userPrint)
+{
+    if(vowels.Contains(item))
+    {
+        count++;
+    }
+}
+
+System.Console.WriteLine(count);    

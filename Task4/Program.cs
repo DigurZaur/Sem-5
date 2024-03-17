139761b6f7cb1c41d742feb3430bf12119ca8e64
Zaurbek Digurov <digurzaur@yandex.ru>
2024-03-17 11:06:59 +0300

    finished Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте массив символов (тип char[]). Создайте
+// строку из символов этого массива
+
+char[] array = new char[5]{'t', 'g', '5', '#', '2'};
+string str = "";
+
+for (int i = 0; i < array.Length; i++)
+{
+    str = str + array[i];
+}
+
+System.Console.WriteLine(str);
+
+foreach (char item in array)
+{
+    
+}
